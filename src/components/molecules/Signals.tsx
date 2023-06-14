51f37adfb77acacd07f3e4135450b5fb7db33be6
ceacademyweb <ceacademy.web@gmail.com>
2023-06-14 10:14:53 -0600

    new home

--- src/components/molecules/Signals.tsx
+++ src/components/molecules/Signals.tsx
@@ -4,8 +4,8 @@ const Signals = () => {
   return (
     <section className='Signals'>
       {/* <h1>WE GOT THE SIGNALS</h1> */}
-      <h1>To become a true Trader</h1>
-      <h2>We got the learning to become a true trader</h2>
+      <h1>We got the learning to become a true trader</h1>
+      <h2>To become a true Trader</h2>
     </section>
   )
 }
